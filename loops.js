d90e8bd434766a16badfc4c8bd22423fdc889253
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-26 10:40:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -9,3 +9,16 @@ function forLoop(array){
   }
   return array
 }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+} 
+
+  do {
+    array.pop
+  } while (array.length > 0 && maybeTrue());
+
+  return array
+  
+}
