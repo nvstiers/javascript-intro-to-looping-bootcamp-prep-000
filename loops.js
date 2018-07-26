13a4c42c1b7d4959fb7a9aa3b19ad8b39aec3ed9
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-26 10:43:04 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -10,6 +10,13 @@ function forLoop(array){
   return array
 }
 
+function whileLoop(number){
+  while (number > 0){
+    console.log(--number)
+  }
+  return "done"
+}
+
 function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
