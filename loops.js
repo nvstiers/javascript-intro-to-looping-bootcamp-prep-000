edde1c9d097621b60b8d5955dc63c80852ea0893
Nathaniel Stiers <nvstiers@gmail.com>
2018-07-26 10:44:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -21,9 +21,8 @@ function doWhileLoop(array){
   function maybeTrue() {
   return Math.random() >= 0.5
 }
-
   do {
-    array.pop
+    array.pop()
   } while (array.length > 0 && maybeTrue());
 
   return array
